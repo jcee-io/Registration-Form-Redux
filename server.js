4bc6b04ed31641e8e5e901059435702136a8997d
Justin <jvcruz@ucdavis.edu>
2018-01-31 20:03:47 -0800

    fixed port for deployment

--- server.js
+++ server.js
@@ -110,5 +110,5 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'dist/index.html'));
 });
  
-const server = app.listen(3000, function() {
+const server = app.listen(process.env.PORT || 3000, function() {
 });
